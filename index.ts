dd71e537525a07e2198fd93c9942aa557752fc01
Mohammed Faragallah <o0frego0o@hotmail.com>
2024-05-11 10:52:41 +0300

    remove update token route

--- index.ts
+++ index.ts
@@ -11,21 +11,6 @@ import "./channels";
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
 
-app.post(
-  "/update-tokens",
-  (req: Request<{ deviceTokens: string[]; subscriberId: string }>, res) => {
-    const { deviceTokens, subscriberId } = req.params;
-
-    return novu.subscribers.setCredentials(
-      subscriberId,
-      PushProviderIdEnum.EXPO,
-      {
-        deviceTokens,
-      }
-    );
-  }
-);
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
